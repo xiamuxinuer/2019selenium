d477ec6c6070b750c8d82f117db23aee3dd8e4b0
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 12:29:54 -0600

    amazon

---
+++ src/test/java/Day1/Day1SeleniumClass.java
@@ -0,0 +1,34 @@
+package Day1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.crypto.spec.PSource;
+
+public class Day1SeleniumClass {
+    public static void main(String[] args) {
+
+
+        //set up chrome driver
+
+        WebDriverManager.chromedriver().setup();
+
+        //create chrome driver object
+        ChromeDriver driver=new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- src/test/java/Day2/BasicNavigation.java
+++ src/test/java/Day2/BasicNavigation.java
@@ -1,26 +1,14 @@
+package Day2;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import javax.crypto.spec.PSource;
-
-public class Day1SeleniumClass {
+public class BasicNavigation {
     public static void main(String[] args) {
 
-
-        //set up chrome driver
-
         WebDriverManager.chromedriver().setup();
-
-        //create chrome driver object
         ChromeDriver driver=new ChromeDriver();
-        //open some website
-        driver.get("http://google.com");
-
-
-
-
-
-
+        driver.get("http://amazon.com");
 
 
 
