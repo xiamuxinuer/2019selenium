0238d11fdddf3b18befc12d82090d48d733138c0
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 15:36:52 -0600

    switch window

---
+++ src/test/java/Day2/JumpToNewWindow.java
@@ -0,0 +1,51 @@
+package Day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+
+public class JumpToNewWindow {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+        Thread.sleep(8000);   //for demo: wait3 seconds and close
+
+
+        String windowHandle=driver.getWindowHandle();
+        System.out.println(windowHandle);
+        //System.out.println(driver.getWindowHandle());
+
+        Set<String> windowHandles= driver.getWindowHandles();//set does not allow duplicates.
+        System.out.println(windowHandles);
+        //System.out.println(driver.getWindowHandles());
+
+for (String windowId:windowHandles){
+    if (windowId.equals(windowHandle)){
+        driver.switchTo().window(windowId);
+    }
+}
+        System.out.println(driver.getCurrentUrl());
+
+
+        // driver.close();   // only old window closed. new window still open.
+
+        //driver.quit();// close all the windows that opened by driver.
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
