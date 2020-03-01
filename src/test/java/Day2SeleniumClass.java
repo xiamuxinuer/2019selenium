34325013482128ae2a7d35d24de57bf6e20f6198
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 10:01:38 -0600

    new project

---
+++ src/test/java/Day2SeleniumClass.java
@@ -0,0 +1,23 @@
+import javax.crypto.spec.PSource;
+
+public class Day2SeleniumClass {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
