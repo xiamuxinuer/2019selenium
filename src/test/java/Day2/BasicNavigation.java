d7d9780108cebc4251b43932b99b0358e4d7db40
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 12:33:24 -0600

    open, sleep,close website

--- src/test/java/Day2/BasicNavigation.java
+++ src/test/java/Day2/BasicNavigation.java
@@ -4,11 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver=new ChromeDriver();
         driver.get("http://amazon.com");
+        Thread.sleep(3000);
+
+        driver.close();
 
 
 
