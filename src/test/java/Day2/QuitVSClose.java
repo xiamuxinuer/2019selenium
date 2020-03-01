238757a6ae87726f578c97934d9ed363650c4da6
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 15:16:03 -0600

    close vs quit

--- src/test/java/Day2/QuitVSClose.java
+++ src/test/java/Day2/QuitVSClose.java
@@ -1,13 +1,19 @@
 package Day2;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuitVSClose {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+        Thread.sleep(10000);   //for demo: wait3 seconds and close
 
+       // driver.close();   // only old window closed. new window still open.
 
-
-
-
+driver.quit();// close all the windows that opened by driver.
 
 
 
