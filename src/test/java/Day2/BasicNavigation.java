b109a676851f689e781ad72a68264bb478ed9b30
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-01 12:54:51 -0600

    google title test

--- src/test/java/Day2/BasicNavigation.java
+++ src/test/java/Day2/BasicNavigation.java
@@ -8,8 +8,20 @@ public class BasicNavigation {
 
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver=new ChromeDriver();
-        driver.get("http://amazon.com");
-        Thread.sleep(3000);
+        driver.get("http://google.com");
+        //Thread.sleep(3000);   //for demo: wait3 seconds and close
+
+
+        String title=driver.getTitle(); //returns title
+        System.out.println("title is "+ title);
+        String expectedTitle="Google";
+        if (expectedTitle.equals(title)) {
+
+            System.out.println("test passed");
+
+        }else {
+            System.out.println("test fail");
+        }
 
         driver.close();
 
