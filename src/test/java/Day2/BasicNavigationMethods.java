7095bd325962a1d13235fba426793208cd7a9ca9
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-02 12:00:09 -0600

    switch window method

--- src/test/java/Day2/BasicNavigationMethods.java
+++ src/test/java/Day2/BasicNavigationMethods.java
@@ -3,7 +3,7 @@ package Day2;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class googletest {
+public class BasicNavigationMethods {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver=new ChromeDriver();
--- src/test/java/Day2/GoogleSearchPractice.java
+++
@@ -1,7 +0,0 @@
-package Day2;
-
-public class GoogleSearchPractice {
-    public static void main(String[] args) {
-
-    }
-}
