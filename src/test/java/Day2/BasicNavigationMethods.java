388797d17321a90359f051e2542962ae447df4b3
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-02 12:33:58 -0600

    navigation method

--- src/test/java/Day2/BasicNavigationMethods.java
+++ src/test/java/Day2/BasicNavigationMethods.java
@@ -16,23 +16,47 @@ public class BasicNavigationMethods {
 
        // driver.manage().window().maximize();
         driver.navigate().to("http://amazon.com");
+
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("test passed");
+        }
+        else {
+            System.out.println("test failed");
+        }
+        Thread.sleep(3000);
+
+//come back to google page:
+        driver.navigate().back();
+
+        Thread.sleep(3000);
+
+
+// go to amazon again then refresh the amazon page
         driver.navigate().forward();
         driver.navigate().refresh();
         Thread.sleep(5000);
-
+// go back to google page:
         driver.navigate().back();
         Thread.sleep(3000);
+
+
+     //call the method:
         verifyEquals(title,"Google");
+
+
+   //go to amazon page :
         driver.navigate().forward();
         Thread.sleep(3000);
-        System.out.println("page title "+driver.getTitle());
+        System.out.println("page title "+driver.getTitle());// page title is :amazon
+        System.out.println("current page url: "+driver.getCurrentUrl());
 
+        //go back to google page:
         driver.navigate().back();
 
         Thread.sleep(3000);
 
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getTitle());// page title is : google
+        System.out.println(driver.getCurrentUrl());// google url : https://www.google.com/?gws_rd=ssl
 
 
 driver.close();
