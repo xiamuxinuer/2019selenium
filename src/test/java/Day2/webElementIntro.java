d29956541b3443f5b461f8d0ceba84e95e31a430
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-03 11:09:04 -0600

    practice google search

--- src/test/java/Day2/webElementIntro.java
+++ src/test/java/Day2/webElementIntro.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class WebElement {
+public class webElementIntro {
     public static void main(String[] args) throws InterruptedException {
 
 
