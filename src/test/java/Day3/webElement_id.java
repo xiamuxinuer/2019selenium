8575a4afa80dd8b6e1654c7d9175015855737c4d
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-03 19:56:05 -0600

    web element practice :name and id

--- src/test/java/Day3/webElement_id.java
+++ src/test/java/Day3/webElement_id.java
@@ -1,5 +1,28 @@
 package Day3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utilities.DriverFactory;
+
 public class webElement_id {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver= DriverFactory.createDriver("chrome");
+
+        driver.get("http://practice.cybertekschool.com/login");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        Thread.sleep(5000);
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword");
+        Thread.sleep(5000);
+        driver.findElement(By.id("wooden_spoon")).click();
+        Thread.sleep(5000);
+
+        driver.quit();
+
+
+
+
+
+
+    }
 
 }
