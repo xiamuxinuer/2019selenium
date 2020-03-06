814897403910d75c7fe9efc464961aabf1d9fee6
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-06 11:58:10 -0600

    ebay, amazon, wikipedia methods calls one after another

--- src/test/java/DAY4/WarmUpTask.java
+++ src/test/java/DAY4/WarmUpTask.java
@@ -1,26 +1,25 @@
 package DAY4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.bytebuddy.asm.Advice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.DriverFactory;
 
 public class WarmUpTask {
-    public static void main(String[] args) {
-
-        DriverFactory.createDriver("chrome");
-        WebDriver driver=new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException {
 
 //call the methods:
 
-      // ebayTask(driver);
+       ebayTask();
 
-      //  amazonTask(driver);
+        amazonTask();
 
 
-        wikipediaTask(driver);
+        wikipediaTask();
 
 
     }
@@ -28,10 +27,12 @@ public class WarmUpTask {
 //enter search term
 //click on search button
 //print number of results
-    public static void  ebayTask(WebDriver driver){
+    public static void  ebayTask(){
+     WebDriver   driver=DriverFactory.createDriver("chrome");
 
         driver.get("http://ebay.com");
         driver.findElement(By.name("_nkw")).sendKeys("java book",Keys.RETURN);
+
         System.out.println(driver.findElement(By.tagName("h1")).getText());
 
         driver.quit();
@@ -39,17 +40,23 @@ public class WarmUpTask {
     }
 
 //go to amazon
-//Go to ebay
 //enter search term
 //click on search button
 //verify title contains search term
-    public static void amazonTask(WebDriver driver){
+    public static void amazonTask() throws InterruptedException {
+        WebDriver   driver=DriverFactory.createDriver("chrome");
         driver.get("http://amazon.com");
 
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("java book",Keys.ENTER);
 
-        boolean result=driver.getTitle().contains("java book");
-        System.out.println(result);
+        Thread.sleep(2000);
+        String title=driver.getTitle();
+        //System.out.println(title);
+       if (title.contains("java book")){
+           System.out.println("true");
+       }else {
+           System.out.println("false");
+       }
         driver.quit();
 
     }
@@ -59,17 +66,14 @@ public class WarmUpTask {
 //click on search button
 //click on search result `Selenium (software)`
 //verify url ends with `Selenium_(software)`
-public static void wikipediaTask(WebDriver driver){
+public static void wikipediaTask(){
+    WebDriver   driver=DriverFactory.createDriver("chrome");
         driver.get("http://wikipedia.org");
         driver.findElement(By.id("searchInput")).sendKeys("selenium webdriver",Keys.ENTER);
         driver.findElement(By.linkText("Selenium (software)")).click();
         boolean result=driver.getCurrentUrl().endsWith("Selenium_(software)");
     System.out.println(result);
     driver.quit();
-
-
-
-
 }
 
 
