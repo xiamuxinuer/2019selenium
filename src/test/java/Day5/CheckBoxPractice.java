cf84c0026b45b691d95337b07eb95889a86b31c1
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-07 12:57:38 -0600

    check box practice  2

--- src/test/java/Day5/CheckBoxPractice.java
+++ src/test/java/Day5/CheckBoxPractice.java
@@ -1,9 +1,13 @@
 package Day5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.BrowserUnits;
+
+import java.util.List;
 
 public class CheckBoxPractice {
     public static void main(String[] args) {
@@ -12,6 +16,36 @@ public class CheckBoxPractice {
 
         driver.get("http://practice.cybertekschool.com/checkboxes");
         BrowserUnits.wait(2);
+        //verify check box1 is not selected, check box 2 is has already selected:
+
+        List<WebElement> checkBoxes=driver.findElements(By.tagName("input"));
+        for (int i = 0; i <checkBoxes.size() ; i++) {
+            if (!checkBoxes.get(i).isSelected()){
+                System.out.println(i+1+" check box is bot selected");
+            }else {
+                System.out.println(i+1+" check box is not selected yet");
+            }
+        }
+
+
+        BrowserUnits.wait(2);
+
+        if (!checkBoxes.get(0).isSelected()&&checkBoxes.get(1).isSelected()){
+            System.out.println("test passed");
+        }else {
+            System.out.println("failed");
+        }
+
+
+
+
+
+
+
+driver.quit();
+
+
+
 
     }
 }
