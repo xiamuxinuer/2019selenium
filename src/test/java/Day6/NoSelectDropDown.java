2391846034f3169d253b0b69221574ae4dda9e71
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-08 12:24:52 -0500

    non selected drop down

---
+++ src/test/java/Day6/NoSelectDropDown.java
@@ -0,0 +1,35 @@
+package Day6;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import utilities.Browserutils;
+
+public class NoSelectDropDown {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver= new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        Browserutils.wait(2);
+       driver.findElement(By.id("dropdownMenuLink")).click();// find drop down and click to expend it and see the options:
+
+        Browserutils.wait(2);
+
+        driver.findElement(By.linkText("Amazon")).click();//find drop down option and click it
+        Browserutils.wait(2);
+        driver.navigate().back();
+        Browserutils.wait(2);
+        driver.findElement(By.id("dropdownMenuLink")).click();
+        Browserutils.wait(2);
+        driver.findElement(By.linkText("Google")).click();
+
+
+
+        Browserutils.wait(2);
+driver.quit();
+    }
+}
