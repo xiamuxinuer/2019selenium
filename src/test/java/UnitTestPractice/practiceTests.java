cffb982dc6bfa9615679406b3074b123be17ee62
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-14 16:25:55 -0500

    test NG practice

--- src/test/java/UnitTestPractice/practiceTests.java
+++ src/test/java/UnitTestPractice/practiceTests.java
@@ -2,6 +2,7 @@ package UnitTestPractice;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -41,12 +42,27 @@ public class practiceTests {
         Thread.sleep(1000);
 
        String  actual=driver.findElement(By.tagName("h4")).getText();
+
        String  expected="Welcome to the Secure Area. When you are done click logout below.";
       Assert.assertEquals(actual,expected,"message is not match");
 
     }
 
+@Test
+    public  void  forgetPasswordTest() throws InterruptedException {
+         driver.findElement(By.linkText("Forgot Password")).click();
+    Thread.sleep(1000);
+         driver.findElement(By.name("email")).sendKeys("efewtrell8c@craigslist.org", Keys.ENTER);
+    Thread.sleep(1000);
+        String actual=driver.findElement(By.tagName("h4")).getText();
+        String expected="Your e-mail's been sent!";
+        Assert.assertEquals(actual,expected,"message is not match");
 
+
+
+
+
+}
 
 
 
