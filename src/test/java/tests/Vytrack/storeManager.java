9d4cf575dc9eee1689cd37386a731f716e433ff3
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-17 21:09:22 -0500

    Activity--log call--create new calendar

--- src/test/java/tests/Vytrack/storeManager.java
+++ src/test/java/tests/Vytrack/storeManager.java
@@ -12,6 +12,10 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class  storeManager{
     private WebDriver driver;
     private  String  url="https://qa2.vytrack.com/user/login";
@@ -23,7 +27,9 @@ public class  storeManager{
 
     Actions  actions;
     private By activitiesBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Activities')]");
-
+    private By currentUserBy = By.cssSelector("#user-menu > a");//[id=user-menu] go to child element use " > a"
+    private By startDateBy = By.cssSelector("[id*='date_selector_oro_calendar_event_form_start-uid']");
+    private By startTimeBy = By.cssSelector("[id*='time_selector_oro_calendar_event_form_start-uid']");
 
     @BeforeMethod
  public void setUp() throws InterruptedException {
@@ -67,5 +73,53 @@ public class  storeManager{
 
 
     }
+//Login as sales manager
+//Go to Activities --> Calendar Events
+//Click on Create Calendar Event
+//Default owner name should be current user
+//Default title should be blank
+//Default start date should be current date
+//Default start time should be current time
+
+    @Test
+    public void defaultOwner() throws InterruptedException {
+        driver.findElement(By.linkText("Calendar Events")).click();
+        Thread.sleep(5000);
+       driver.findElement(By.cssSelector("a[title='Create Calendar event']")).click();
+        Thread.sleep(5000);
+       String currentUser= driver.findElement(currentUserBy).getText().trim();
+       String defaultUser=driver.findElement(By.cssSelector("span[class='select2-chosen']")).getText().trim();
+       Assert.assertEquals(defaultUser,currentUser);
+
+       WebElement defaultTitle=driver.findElement(By.name("oro_calendar_event_form[title]"));//find input title box
+       Assert.assertTrue(defaultTitle.getAttribute("value").isEmpty());
+
+
+       WebElement startDate=driver.findElement(By.cssSelector("[id*='date_selector_oro_calendar_event_form_start-uid']"));
+       String actualDay=startDate.getAttribute("value");
+       String expectedDate= LocalDate.now().format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+       Assert.assertEquals(actualDay,expectedDate);
+
+       WebElement startTime=driver.findElement(startTimeBy);
+       String  actualTime=startTime.getAttribute("value");
+       String  expectedTime= LocalTime.now().format(DateTimeFormatter.ofPattern("h:m a"));
+       Assert.assertEquals(actualTime,expectedTime,"time is not correct");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
