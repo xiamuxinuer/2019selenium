bafb2281a91572be23192cfa49f251193ac449cc
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-18 20:45:39 -0500

    scroll down practice

---
+++ src/test/java/Day9/JSExecutor.java
@@ -0,0 +1,44 @@
+package Day9;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Set;
+
+public class JSExecutor  {
+
+    private RemoteWebDriver driver;
+
+  @BeforeMethod
+  public void setUp(){
+      WebDriverManager.chromedriver().version("79").setup();
+      driver=new ChromeDriver();
+
+  }
+
+  @AfterMethod
+  public void tearDown() {
+      driver.quit();
+  }
+
+  @Test
+  public void scrollTest() throws InterruptedException {
+      driver.get("http://practice.cybertekschool.com/infinite_scroll");
+      Thread.sleep(3000);
+
+
+      driver.executeScript("window.scrollBy(0,250)");
+      Thread.sleep(3000);
+  }
+
+
+}
