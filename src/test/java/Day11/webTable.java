1e9a7ac2f5c2d856e08110343c45fc9d85114130
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-21 15:57:07 -0500

    check if element not exist

--- src/test/java/Day11/webTable.java
+++ src/test/java/Day11/webTable.java
@@ -41,10 +41,47 @@ public class webTable {
 
     }
 
+@Test
+    public void verifyRowCount(){
+       List<WebElement> rows=driver.findElements(By.xpath("//table[1]//tbody//tr"));
+    System.out.println(rows.size());  // number of rows
+    Assert.assertEquals(rows.size(),4);
+}
+
+@Test
+    public void getSpecificColumn(){
+        //td[5] is column with links:
+        List<WebElement> allLinks= driver.findElements(By.xpath("//table[1]//tbody//tr//td[5]"));
+    //System.out.println(utilities.webTable.getTextFromWebElements(allLinks)); // give a list of string
+
+    //or for each loop to print out each link separately:
+    for (WebElement eachLink:allLinks){
+        System.out.println(eachLink.getText());
+    }
+}
 
 
+    /** TASK until 4:45
+     * Go to tables example page
+     * Delete record with jsmith@gmail.com email
+     * verify that number of rows is equals to 3
+     * verify that jsmith@gmail.com doesn't exists any more in the table
+     */
+@Test
+    public void deleteRecord(){
+    driver.findElement(By.xpath("//table[1]//tbody//tr[1]//td[5]//following-sibling::td/a[2]")).click();
+    List<WebElement> rows=driver.findElements(By.xpath("//table[1]//tbody//tr"));
 
+    Assert.assertEquals(rows.size(),3);
 
+   Assert.assertTrue(driver.findElements(By.xpath("//*[@id=\"table1\"]/tbody/tr[1]/td[text()='jsmith@gmail.com']")).isEmpty());
+//or
+    List<WebElement> elements=driver.findElements(By.xpath("//*[@id=\"table1\"]/tbody/tr[1]/td[text()='jsmith@gmail.com']"));
+    Assert.assertTrue(elements.isEmpty());
+    //or
+    System.out.println(elements.size());
+    Assert.assertTrue(elements.size()==0);
+}
 
 
 
