11bafe43d408bb8445e8e653e2e6389865e98a73
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-22 12:50:55 -0500

    explicit wait practice

---
+++ src/test/java/Day12/explicitWaitPractice.java
@@ -0,0 +1,40 @@
+package Day12;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.DriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class explicitWaitPractice {
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    @BeforeMethod
+    public void setUp(){
+        driver= DriverFactory.createDriver("chrome");
+
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void waitForTitle(){
+        driver.get("http://google.com");
+        WebDriverWait wait= new WebDriverWait(driver,10);
+        //wait up to 10 seconds until title is google:
+        wait.until(ExpectedConditions.titleIs("Google"));
+
+    }
+
+}
