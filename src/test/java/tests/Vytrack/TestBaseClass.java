8e7188299f237e4631dd99dedaa3572e092e93e3
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-03-28 11:08:09 -0500

    Test Base Class: @Before method and @After method has created

---
+++ src/test/java/tests/Vytrack/TestBaseClass.java
@@ -0,0 +1,27 @@
+package tests.Vytrack;
+
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public  abstract class TestBaseClass {
+    protected WebDriverWait wait;
+    protected Actions actions;
+    @BeforeMethod
+    public void setUp(){
+        String url= ConfigurationReader.getProperty("qa1");
+        Driver.getDriver().get(url);
+        Driver.getDriver().manage().window().maximize();
+        wait=new WebDriverWait(Driver.getDriver(),15);
+        actions=new Actions(Driver.getDriver());
+    }
+
+     @AfterMethod
+    public void tearDown(){
+        Driver.closeDriver();
+     }
+
+}
