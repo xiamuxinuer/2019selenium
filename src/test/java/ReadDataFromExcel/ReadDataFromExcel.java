7cc09af858a4290bce4b6984e52a5ec3e1acbb5c
xiamuxinuer <emmaxiaogaiti@gmail.com>
2020-04-07 21:03:03 -0500

    excel file reading

--- src/test/java/ReadDataFromExcel/ReadDataFromExcel.java
+++ src/test/java/ReadDataFromExcel/ReadDataFromExcel.java
@@ -1,9 +1,12 @@
 package ReadDataFromExcel;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import org.apache.poi.ss.usermodel.*;
 import org.testng.annotations.Test;
+import utilities.ExcelUtils;
 
 import java.io.File;
+import java.util.Map;
 
 public class ReadDataFromExcel {
     @Test
@@ -20,9 +23,58 @@ public class ReadDataFromExcel {
         Cell firstCell = firstRow.getCell(0);
         //get string value
         String value = firstCell.getStringCellValue();
+        String secondCellValue=firstRow.getCell(1).getStringCellValue();
+        //System.out.println(firstRow.getCell(1));
 
         System.out.println(value);
+        System.out.println(secondCellValue);
+
+        int lastCell=firstRow.getLastCellNum();
+
+        System.out.println("###############");
+
+        for (int i = 0; i <lastCell; i++) {
+            System.out.print(firstRow.getCell(i)+"|");
+
+        }
+        System.out.println("################");
+
+      int numberOFRows=workSheet.getLastRowNum();//index of last row
+      int numberOfRows2=workSheet.getPhysicalNumberOfRows();
+        System.out.println("index of last row :"+numberOFRows);
+        System.out.println("total number of rows :"+ numberOfRows2);
+
+
+        for (int row = 0; row <workSheet.getPhysicalNumberOfRows(); row++) {
+            for (int cell = 0; cell <workSheet.getRow(row).getLastCellNum(); cell++) {
+                String cellValue=workSheet.getRow(row).getCell(cell).getStringCellValue();
+                System.out.print(cellValue+" | ");
+            }
+            System.out.println();
+        }
+
     }
+
+    @Test
+    public void ExcelUtilityTest(){
+        String path="VytrackTestUsers.xlsx";
+        String spreadSheet="QA1-all";
+        ExcelUtils excelUtils=new ExcelUtils(path,spreadSheet);
+       for (Map<String,String> record:excelUtils.getDataList()){
+           System.out.println(record);
+       }
+
+
+       // excelUtils.getDataList().forEach(System.out::println);
+    }
+
+
+
+
+
+
+
+
 
 }
 
